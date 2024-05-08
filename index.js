102b8f8a818d5f583a4effe7d313698aa3a0ee40
mohammed-iqyan-15052002 <iqyanmohammed@gmail.com>
2024-05-08 19:22:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let a=document.getElementById('i1')
+let b=document.getElementById('i2')
+let c=document.getElementById('btn4')
+let d=document.getElementById('for1')
+let e=document.getElementById('for2')
+let f=document.getElementById('btn2')
+let g=document.getElementById('h')
+let h=document.getElementById('he')
+c.style.display='none'
+e.style.display='none'
+h.style.display='none'
+f.addEventListener('click', ()=>{
+    e.style.display='block'
+    f.style.display='none'
+    g.style.display='none'
+    h.style.display='block'
+    c.style.display='block'
+    e.style.display='flex'
+    d.style.display='none'
+    
+})
+c.addEventListener('click',()=>{
+    e.style.display='none'
+    d.style.display='block'
+    c.style.display='none'
+    f.style.display='block'
+    g.style.display='block'
+    h.style.display='none'
+    d.style.display='flex'
+})
